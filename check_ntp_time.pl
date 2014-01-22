4894d365c4efc768178967ada13213bd9273aa2a
John Kinsella <jlk@stratosec.co>
2014-01-22 13:29:54 -0800

    Converting DOS-style to UNIX CR/LF

--- check_ntp_time.pl
+++ check_ntp_time.pl
@@ -2,9 +2,9 @@
 #--------------------------------------------------------------------------------------------------------------
 #
 # Program Name: check_ntp_time.pl
-# Purpose:      Check if the server clock is in syncronization with the NTP server clock
-# Date:         15/09/2011
-# Author:       Antonio Fernando Evangelista
+# Purpose:	Check if the server clock is in syncronization with the NTP server clock
+# Date: 	15/09/2011
+# Author:	Antonio Fernando Evangelista
 #
 #--------------------------------------------------------------------------------------------------------------
 
@@ -18,9 +18,9 @@ sub usage() {
   print "usage:\n";
   print " check_ntp_time.pl <-H ntpserver> <-w warnlevel> <-c critlevel>\n\n";
   print "options:\n";
-  print " -H ntpserver   Hostname or IP address of the ntp server\n";
-  print " -w seconds     Difference in seconds from the ntpserver clock and the server clock which the plugin will emit a warning alarm - Default = 150 seconds (above or below)\n";
-  print " -c seconds     Difference in seconds from the ntpserver clock and the server clock which the plugin will emit a critical alarm - Default = 300 seconds (above or below)\n";
+  print " -H ntpserver	 Hostname or IP address of the ntp server\n";
+  print " -w seconds	 Difference in seconds from the ntpserver clock and the server clock which the plugin will emit a warning alarm - Default = 150 seconds (above or below)\n";
+  print " -c seconds	 Difference in seconds from the ntpserver clock and the server clock which the plugin will emit a critical alarm - Default = 300 seconds (above or below)\n";
   print "\nCopyright (C) 2011 Antonio Fernando Evangelista <afevangelista\@uol.com.br>\n";
   print "check_ntp_time.pl comes with absolutely NO WARRANTY either implied or explicit\n";
   print "This program is licensed under the terms of the\n";
@@ -99,36 +99,36 @@ my $offset =(($ntp{'Receive Timestamp'} - $ntp{'Originate Timestamp'}) + ($ntp{'
 
      if($offset >=0 )
      {
-         if ( $offset >= $criticalpos )
-         {
-             printf ("NTP CRITICAL: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningpos, $criticalpos);
-             exit 2;
-         }
-         elsif ( $offset >= $warningpos )
-         {
-             printf ("NTP WARNING: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningpos, $criticalpos);
-             exit 1;
-         }
-         else
-         {
-             printf ("NTP OK: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningpos, $criticalpos);
-             exit 0;
-         }
+	 if ( $offset >= $criticalpos )
+	 {
+	     printf ("NTP CRITICAL: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningpos, $criticalpos);
+	     exit 2;
+	 }
+	 elsif ( $offset >= $warningpos )
+	 {
+	     printf ("NTP WARNING: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningpos, $criticalpos);
+	     exit 1;
+	 }
+	 else
+	 {
+	     printf ("NTP OK: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningpos, $criticalpos);
+	     exit 0;
+	 }
      }
 
      if ( $offset <= $criticalneg )
      {
-         printf ("NTP CRITICAL: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningneg, $criticalneg);
-         exit 2;
+	 printf ("NTP CRITICAL: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningneg, $criticalneg);
+	 exit 2;
      }
      elsif ( $offset <= $warningneg )
      {
-         printf ("NTP WARNING: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningneg, $criticalneg);
-         exit 1;
+	 printf ("NTP WARNING: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningneg, $criticalneg);
+	 exit 1;
      }
      else
      {
-         printf ("NTP OK: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningneg, $criticalneg);
-         exit 0;
+	 printf ("NTP OK: Offset %.6f secs|offset=%.6fs;%.6f;%.6f;\n", $offset, $offset, $warningneg, $criticalneg);
+	 exit 0;
      }
 
